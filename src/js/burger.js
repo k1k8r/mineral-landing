83564bd9ebb5c8efef8ec7dee1bf19c1e35adca9
Artem4590 <lavryha4590@gmail.com>
2021-05-03 14:47:47 +0300

    Внес изменения и поставил якорь на форму

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,19 +1,25 @@
 const headerBurger = document.querySelector('.header-burger');
 const headerNav = document.querySelector('.header-nav');
-const headerLink =  document.querySelectorAll('.header-menu__link');
+const headerLink = document.querySelectorAll('.header-menu__link');
+const formLink = document.querySelector('.header_form-link');
 const body = document.querySelector('body');
 
 headerBurger.addEventListener('click', (e) => {
-	e.target.classList.toggle('active');
-	headerNav.classList.toggle('active');
-	body.classList.toggle('lock');
+    e.target.classList.toggle('active');
+    headerNav.classList.toggle('active');
+    body.classList.toggle('lock');
+});
+
+formLink.addEventListener('click', () => {
+    headerBurger.classList.remove('active');
+    headerNav.classList.remove('active');
+    body.classList.remove('active');
 });
 
 headerLink.forEach(link => {
-	link.addEventListener('click', (e) => {
-		headerBurger.classList.remove('active');
-		headerNav.classList.remove('active');
-		body.classList.remove('active');
-	});
+    link.addEventListener('click', (e) => {
+        headerBurger.classList.remove('active');
+        headerNav.classList.remove('active');
+        body.classList.remove('active');
+    });
 });
-
